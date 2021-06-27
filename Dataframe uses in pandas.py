d9c881ab00fe30f67653d84ebd984a287e375b83
Bittu raj <67989399+Bitturaj391@users.noreply.github.com>
2021-06-27 22:58:30 +0530

    Add files via upload

---
+++ Dataframe uses in pandas.py
@@ -0,0 +1,7 @@
+dict = {"country":["Brazil","Russia","India","China","South africa"],
+        "capital":["Brapillia","Moscow","New Delhi","Bejieng","Pretoria"],
+        "area":[8.519,17.10,3.286,9.596,1.721],
+        "population":[200.4,143.5,1252,1357,52.98]}
+import pandas as pd
+brics = pd.DataFrame(dict)
+brics
